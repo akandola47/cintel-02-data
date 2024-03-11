93054d138348297fc96ce8133470875bfee9f83b
akandola47 <143216836+akandola47@users.noreply.github.com>
2024-03-11 13:46:24 -0500

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shinywidgets import render_plotly
+import palmerpenguins
+
+ui.page_opts(title="Filling layout", fillable=True)
+with ui.layout_columns():
+
+    @render_plotly
+    def plot1():
+        return px.histogram(px.data.tips(), y="tip")
+
+    @render_plotly
+    def plot2():
+        return px.histogram(px.data.tips(), y="total_bill")
+
+
+
+
+penguins_df = palmerpenguins.load_penguins()
+
+
+ui.page_opts(title= "Arsh penguins", fillable=True)
